5cd707f399588550c3e1056f2ddcbf0fc6108c90
laavanayadhawan <48582727+laavanayadhawan@users.noreply.github.com>
2019-10-20 09:48:32 +0530

    #12

--- Program6.cpp
+++ Program6.cpp
@@ -9,7 +9,7 @@ void reverse()
   
         while (node1 != NULL) {
             node3= node1->next;
-            node1->next = node2;
+            node3->next = node2;
             node2= node1;
             node1= node1->next; 
         }
