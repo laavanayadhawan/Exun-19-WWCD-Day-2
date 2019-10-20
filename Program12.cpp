1b1e87431b2267c18b7979fb48341ebba4e7bce5
laavanayadhawan <48582727+laavanayadhawan@users.noreply.github.com>
2019-10-20 10:18:43 +0530

    Update Program12.cpp

--- Program12.cpp
+++ Program12.cpp
@@ -5,6 +5,7 @@ int function(int n) {
     int temp = n,value = 0, c = 1; 
     while (temp) { 
         int r = temp % 10;  
+        temp=temp/10;
         value += r * c; 
         c *= 2; 
     } 
